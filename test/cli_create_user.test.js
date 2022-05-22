714d6a0403ebf37399128d5c7d633debbd5de7aa
9bany <bany.y0599@gmail.com>
2022-05-22 22:47:54 +0700

    user creation, username empty

--- test/cli_create_user.test.js
+++ test/cli_create_user.test.js
@@ -1,6 +1,7 @@
 
 const assert = require('assert');
-const { USER_CREATED } = require('../src/constants/global');
+const { USER_CREATED, USERNAME_EMPTY } = require('../src/constants/global');
+const { toMessage } = require('../src/utils/binding');
 const { runCommandWithGit, randomUser } = require('./util');
 
 let testCases = [
@@ -19,6 +20,23 @@ let testCases = [
             assert.notEqual(stringData, null)
             assert.notEqual(stringData, undefined)
             assert.equal(stringData.includes("ssh-keygen-lite"), true)
+        }
+    },
+    {
+        name:'username empty',
+        stub: (check)=> {
+            let user = randomUser()
+            return runCommandWithGit(`-a.username -a.email=${user.email}`).then(data => {
+                check(data)
+            })
+        },
+        check:(data) => {
+            let stringData = data.toString('utf8')
+            console.log(stringData)
+            assert.notEqual(stringData, '')
+            assert.notEqual(stringData, null)
+            assert.notEqual(stringData, undefined)
+            assert.equal(stringData.includes(toMessage(USERNAME_EMPTY)), true)
         }
     },
     
