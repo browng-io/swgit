e8b84630574e9d0f6cf7f3f2daf79049de4ec9e1
9bany <bany.y0599@gmail.com>
2022-05-22 22:01:49 +0700

    add version test

--- test/bin.test.js
+++ test/bin.test.js
@@ -1,27 +1,5 @@
-const { spawn } = require('child_process');
+
 const assert = require('assert');
-
-function runCommandWithGit(command) {
-    return new Promise((resolve, reject) => {  
-        let arrayCommand = command.split(" ")
-        var listen = spawn(`node`, ['./bin/index.js'].concat(arrayCommand));
-
-        listen.stdout.on('data', function(a){
-            resolve(a)
-            return;
-        });
-    
-        listen.on('exit',function(){
-            resolve('exited')
-            return;
-        })
-    
-        listen.stderr.on('data',function(a){
-            resolve(a)
-            return;
-        });
-    })
-}
 
 let testCases = [
     {
@@ -35,6 +13,22 @@ let testCases = [
             let stringData = data.toString('utf8')
             assert.notEqual(stringData, '')
             assert.notEqual(stringData, null)
+            assert.notEqual(stringData, undefined)
+        }
+    },
+    {
+        name:'version',
+        stub: (check)=> {
+            return runCommandWithGit('--version').then(data => {
+                check(data)
+            })
+        },
+        check:(data) => {
+            let stringData = data.toString('utf8')
+            console.log(stringData)
+            assert.equal(stringData.includes("v"), true)
+            assert.notEqual(stringData, null)
+            assert.notEqual(stringData, '')
             assert.notEqual(stringData, undefined)
         }
     }
