a04cb0b45409b8a64805055fbccb3a2f0c376b25
9bany <bany.y0599@gmail.com>
2022-05-24 21:44:02 +0700

    add more case for user deletion

--- test/cli_delete.test.js
+++ test/cli_delete.test.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const { USER_DOES_NOT_EXISTS, USER_DELETED } = require('../src/constants/global');
+const { USER_DOES_NOT_EXISTS, USER_DELETED, USERNAME_EMPTY } = require('../src/constants/global');
 const { toMessage } = require('../src/utils/binding');
 const runCreateUserTest = require('./create_user.test');
 const { runCommandWithGit, randomUser } = require('./util');
@@ -23,7 +23,51 @@ runCreateUserTest(user).then(data => {
                 assert.equal(userResult.includes(USER_DELETED), true)
             }
         },
-        
+        {
+            name: "Username invalid '' ",
+            stub: (check) => {
+                return runCommandWithGit(`-d=${''}`).then(data => {
+                    check(data)
+                })
+            },
+            check: (data) => {
+                var userResult = data.toString('utf8')
+                assert.notEqual(userResult, '')
+                assert.notEqual(userResult, null)
+                assert.notEqual(userResult, undefined)
+                assert.equal(userResult.includes(USERNAME_EMPTY), true)
+            }
+        },
+        {
+            name: "Username invalid null ",
+            stub: (check) => {
+                return runCommandWithGit(`-d=${null}`).then(data => {
+                    check(data)
+                })
+            },
+            check: (data) => {
+                var userResult = data.toString('utf8')
+                assert.notEqual(userResult, '')
+                assert.notEqual(userResult, null)
+                assert.notEqual(userResult, undefined)
+                assert.equal(userResult.includes(USERNAME_EMPTY), true)
+            }
+        },
+        {
+            name: "Username invalid undifined ",
+            stub: (check) => {
+                return runCommandWithGit(`-d=${undefined}`).then(data => {
+                    check(data)
+                })
+            },
+            check: (data) => {
+                var userResult = data.toString('utf8')
+                assert.notEqual(userResult, '')
+                assert.notEqual(userResult, null)
+                assert.notEqual(userResult, undefined)
+                assert.equal(userResult.includes(USERNAME_EMPTY), true)
+            }
+        },
     ]
     
     describe('cli:get user info', function () { 
