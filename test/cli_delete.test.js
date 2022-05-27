180ba08d75abdc572c50ceda95f859276f9dc61b
9bany <bany.y0599@gmail.com>
2022-05-27 10:00:01 +0700

    ft/test-case-delete-cli-with-full-option-name

--- test/cli_delete.test.js
+++ test/cli_delete.test.js
@@ -1,10 +1,11 @@
 const assert = require('assert');
-const { USER_DOES_NOT_EXISTS, USER_DELETED, USERNAME_EMPTY } = require('../src/constants/global');
+const { USER_DELETED } = require('../src/constants/global');
 const { toMessage } = require('../src/utils/binding');
 const runCreateUserTest = require('./create_user.test');
 const { runCommandWithGit, randomUser } = require('./util');
 
 let user = randomUser();
+let user2 = randomUser();
 
 runCreateUserTest(user).then(data => {
     let testCases = [
@@ -75,3 +76,75 @@ runCreateUserTest(user).then(data => {
         })
     })
 })
+
+
+runCreateUserTest(user2).then(data => {
+    let testCases = [
+        {
+            name: "OK",
+            stub: (check) => {
+                return runCommandWithGit(`--delete=${data.username}`).then(data => {
+                    check(data)
+                })
+            },
+            check: (data) => {
+                var userResult = data.toString('utf8')
+                assert.notEqual(userResult, '')
+                assert.notEqual(userResult, null)
+                assert.notEqual(userResult, undefined)
+                assert.equal(userResult.includes(USER_DELETED), true)
+            }
+        },
+        {
+            name: "Username invalid '' ",
+            stub: (check) => {
+                return runCommandWithGit(`--delete=${''}`).then(data => {
+                    check(data)
+                })
+            },
+            check: (data) => {
+                var userResult = data.toString('utf8')
+                assert.notEqual(userResult, '')
+                assert.notEqual(userResult, null)
+                assert.notEqual(userResult, undefined)
+            }
+        },
+        {
+            name: "Username invalid null ",
+            stub: (check) => {
+                return runCommandWithGit(`--delete=${null}`).then(data => {
+                    check(data)
+                })
+            },
+            check: (data) => {
+                var userResult = data.toString('utf8')
+                assert.notEqual(userResult, '')
+                assert.notEqual(userResult, null)
+                assert.notEqual(userResult, undefined)
+            }
+        },
+        {
+            name: "Username invalid undifined ",
+            stub: (check) => {
+                return runCommandWithGit(`--delete=${undefined}`).then(data => {
+                    check(data)
+                })
+            },
+            check: (data) => {
+                var userResult = data.toString('utf8')
+                assert.notEqual(userResult, '')
+                assert.notEqual(userResult, null)
+                assert.notEqual(userResult, undefined)
+            }
+        },
+    ]
+    
+    describe('cli:get user info with full option names', function () { 
+        testCases.forEach(element => {
+            it(element.name, function () {
+                return element.stub(element.check);
+            })
+        })
+    })
+})
+
