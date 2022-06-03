ec69f2bbd1d5e8f82745146a4911b14138218862
olivanderwood <sunofdark1412@gmail.com>
2022-06-03 10:43:38 +0700

    bugs/show-error-switch-username-not-exist

--- src/controllers/options/switch_user.js
+++ src/controllers/options/switch_user.js
@@ -11,13 +11,13 @@ const log = require('./../../utils/log');
 async function switchUser(username) {
     const store = new Store(db)
     if (!Boolean(username)) {
-        log.debug.error(USERNAME_EMPTY)
+        log.user.error(USERNAME_EMPTY)
         return USERNAME_EMPTY
     }
 
     const userExists = store.getUser(username)
     if(!Boolean(userExists)) {
-        log.debug.error(USER_DOES_NOT_EXISTS)
+        log.user.error(USER_DOES_NOT_EXISTS)
         return USER_DOES_NOT_EXISTS
     }
     
